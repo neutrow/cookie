c824137caea95446d522018b38522fcc37942bb9
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2025-07-09 20:00:05 +0200

    i18n & fonts source

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,36 @@
 // @ts-check
 import starlight from '@astrojs/starlight'
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: 'Reactor Docs',
+      disable404Route: true,
       social: [
         {
           icon: 'github',
           label: 'GitHub',
           href: 'https://github.com/reactor-mc'
+        },
+        {
+          icon: 'discord',
+          label: 'Discord',
+          href: 'https://discord.reactor.ink'
         }
       ],
+      defaultLocale: 'en',
+      locales: {
+        en: {
+          label: 'English',
+          lang: 'en'
+        },
+        es: {
+          label: 'Español',
+          lang: 'es'
+        }
+      },
       sidebar: [
         {
           label: 'Guides',
@@ -26,7 +43,25 @@ export default defineConfig({
           label: 'Reference',
           autogenerate: { directory: 'reference' }
         }
-      ]
+      ],
+      components: {
+        Head: './src/components/Head.astro'
+      },
+      customCss: ['./src/styles/custom.css']
     })
-  ]
+  ],
+  experimental: {
+    fonts: [
+      {
+        cssVariable: '--font-be-vietnam-pro',
+        name: 'Be Vietnam Pro',
+        provider: fontProviders.fontsource()
+      },
+      {
+        cssVariable: '--font-jetbrains-mono',
+        name: 'JetBrains Mono',
+        provider: fontProviders.fontsource()
+      }
+    ]
+  }
 })
